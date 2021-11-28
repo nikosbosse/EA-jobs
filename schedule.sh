751a0b9a26ae68d39b1b87c298972eb0d3dd6352
nikosbosse <nikosbosse@gmail.com>
2021-11-28 18:43:32 +0100

    update repo

--- schedule.sh
+++ schedule.sh
@@ -1,7 +1,7 @@
 #!bin/bash
-gcloud scheduler jobs create pubsub ea-job \
+gcloud scheduler jobs update pubsub ea-job \
     --project=ea-jobs \
-    --schedule="0 6-21/3 * * *" \
+    --schedule="10/30 6-21 * * *" \
     --topic=ea-job \
     --description="Post an EA job offer" \
     --message-body="Post an EA job offer"
